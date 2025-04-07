4c07f24e9a3202e1942d967062fb490432983007
farukycgl <faruk.kuy@gmail.com>
2025-04-07 19:00:40 +0300

    requirements completed

---
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -0,0 +1,47 @@
+package com.workintech.fswebs17d1.controller;
+
+import com.workintech.fswebs17d1.entity.Animal;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.*;
+
+@RestController
+@RequestMapping("/workintech/animal")
+public class AnimalController {
+    private Map<Integer, Animal> animals = new HashMap<>();
+
+    @Value("${course.name}")
+    private String courseName;
+
+    @Value("${project.developer.fullname}")
+    private String developerName;
+
+    @GetMapping
+    public List<Animal> getAllAnimal(){
+        return new ArrayList<>(animals.values());
+    }
+
+    @GetMapping("/{id}")
+    public Animal getAnimalById(@PathVariable int id){
+        return animals.get(id);
+    }
+
+    @PostMapping
+    public Animal addAnimal(@RequestBody Animal animal){
+        animals.put(animal.getId(), animal);
+        return animal;
+    }
+
+    @PutMapping("/{id}")
+    public Animal updateAnimal(@PathVariable int id, @RequestBody Animal updatedAnimal){
+        animals.put(id, updatedAnimal);
+        return updatedAnimal;
+    }
+
+    @DeleteMapping("/{id}")
+    public Animal deleteAnimal(@PathVariable int id){
+        return animals.remove(id);
+    }
+}
